86f62d2023a3352dbd771f9e71f3ec887e6429ef
Andreas <mail@andreaskuster.ch>
2020-01-26 11:56:15 -0800

    Set up continuous integration.

---
+++ testing.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+    Tutty Crawler
+    Copyright (C) 2020  Andreas Kuster
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__description__ = "Continuous integration unit testing."
+__author__ = "Andreas Kuster"
+__copyright__ = "Copyright 2020, Tutti Crawler"
+__license__ = "GPL"
+
+import unittest
+
+
+class Dummy(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(42, 7*6)
+
+
+if __name__ == "__main__":
+    # run all tests
+    unittest.main()
