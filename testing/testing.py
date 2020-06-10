c34fcb16a5f75f3b7786f2e7725b93097f9bc95d
andreaskuster <mail@andreaskuster.ch>
2020-06-10 23:51:36 +0200

    Add adcrawler test cases with 100% coverage.

--- testing/testing.py
+++ testing/testing.py
@@ -24,14 +24,56 @@ __author__ = "Andreas Kuster"
 __copyright__ = "Copyright 2020, Tutti Crawler"
 __license__ = "GPL"
 
+
 import unittest
+import os
+import threading
 
 from adcrawler.helper import zip_to_city
+from adcrawler.bots import EmailBot, TelegramBot
+from adcrawler import AdCrawler
 
 
 class Helper(unittest.TestCase):
+
     def test_zip_to_city(self):
         self.assertEqual("Gossau SG", zip_to_city("9200"))
+
+
+class Crawler(unittest.TestCase):
+
+    def test_crawler_blocking(self):
+        # instantiate crawler
+        crawler = AdCrawler()
+        # run blocking poll function in a background thread
+        polling_thread = threading.Thread(target=crawler.poll, args=(True,))
+        polling_thread.start()
+        # finalize
+        crawler.finalize()
+        # wait for the background thread to terminate
+        polling_thread.join()
+        # remove generated config
+        self.remove_config()
+
+    def test_load_store(self):
+        # instantiate crawler
+        crawler = AdCrawler()
+        # clear old configurations
+        self.remove_config()
+        # load config: case no config
+        crawler.load_data()
+        # store config
+        crawler.store_data()
+        # load config: case config available
+        crawler.load_data()
+        # clean up
+        self.remove_config()
+
+    def remove_config(self, data_path: str = os.path.join("data.json")):
+        # check if the file exists
+        if os.path.isfile(data_path):
+            # delete file
+            os.remove(data_path)
 
 
 if __name__ == "__main__":  # pragma: no cover
